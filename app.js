7d15d03e2a8e75245c5be93e6e4df5dceb9c7416
rajivgalva <rajivgalva@hotmail.com>
2018-02-08 18:02:57 -0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,78 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const session = require('express-session');
+
+
+var db
+var sess;
+
+MongoClient.connect('mongodb://admin:passgen@ds229918.mlab.com:29918/passgen', (err, client) => {
+  if (err) return console.log(err)
+db = client.db('passgen') // whatever your database name is
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('listening on 3000')
+})
+
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(session({secret: 'username'}));
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('listening on 3000')
-  })
 
-  app.get('/', (req, res) => {
-    res.render('index');
-   // res.sendFile(__dirname + '/index.pug')
+
+app.get('/', (req, res) => {
+  res.render('index');
+  // res.sendFile(__dirname + '/index.pug')
+})
+
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  db.collection('users').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
   })
+})
+
+app.post('/login', (req, res) => {
+  sess=req.session;
+  
+  console.log(req.body)
+  db.collection('users').find().toArray(function(err, results) {
+    var length = results.length;
+    for(var i=0; i<length; i++){
+     if ((req.body.loginname === results[i].username) && (req.body.loginpass === results[i].password))
+     {
+      console.log("YAAAY")
+      res.redirect('/dashboard')
+      sess.uname = req.body.loginname;
+    }
+    }
+       
+  })
+})
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+  // res.sendFile(__dirname + '/index.pug')
+})
+
+app.get('/error', (req, res) => {
+  //res.render('dashboard');
+   res.sendFile(__dirname + '/error.html')
+})
+
+app.post('/add', (req, res) => {
+  db.collection('passwords').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+   // res.redirect('/dashboard')
+  })
+})
---
+++ public/js/generate.js
@@ -0,0 +1,14 @@
+function generate() {
+    passgen.passwordsol.value = randomPassword(15);
+}
+
+function randomPassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
+    document.getElementById("password").value = pass;
+    return pass;
+}
